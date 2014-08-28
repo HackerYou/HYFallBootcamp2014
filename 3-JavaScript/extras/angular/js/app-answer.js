1f89165b02c1543a525befc495a09ffb86bf09e6
Brenna <info@brennaobrien.com>
2014-08-28 14:24:52 -0400

    add angular lesson to JS extras

---
+++ 3-JavaScript/extras/angular/js/app-answer.js
@@ -0,0 +1,6 @@
+// create a new controller and pass it the $scope variable
+var MyController = function($scope){
+
+  //$scope holds the values for all our models, so set an initial value on the qty model
+  $scope.qty = 1;
+};
